124eb982cb848d501f5100fc3faa268e8a4995df
Souptik_Banerjee <Souptik_Banerjee@Dell.com>
2020-01-09 14:40:35 +0530

    template driven forms

--- store-app-forms/src/app/product-form/product-form.component.ts
+++ store-app-forms/src/app/product-form/product-form.component.ts
@@ -8,7 +8,30 @@ import { NgForm } from '@angular/forms';
 })
 export class ProductFormComponent implements OnInit {
   showMessage = false;
+
+  @ViewChild('f', {static: false}) form: NgForm;
   constructor() { }
 
+  product: Product = {
+    id: 1,
+    name: "Banana",
+    description: "Banana",
+    price: 1000,
+    isAvailable: true
+  }
+
   ngOnInit() { }
+
+  onSubmit(){
+    console.log("Product info submitted: ", this.form.value);
+    this.form.reset();
+  }
+}
+
+class Product{
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  isAvailable: boolean;
 }
